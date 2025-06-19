dd581f42bc90e23d6cce3d10d856880da80c2376
Rodrigo Tapia <RODRIGOROSALESTAPIA@GMAIL.COM>
2025-06-18 22:31:04 -0300

    ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da agência: ");
+        String agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o número da conta: ");
+        String numeroConta = scanner.nextLine();
+        var nomeCliente = "Mario de Andrade";
+        double saldo = 237.48;
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " +numeroConta + " e seu saldo R$ " + saldo + " já está disponível para saque.");
+
+
+        scanner.close();
     }
 }
